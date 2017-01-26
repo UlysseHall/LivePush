0826824bc0b1dadcb9bf5c90c10f31074d6ff89b
UlysseHall <ulysse.hall@hetic.net>
2017-01-26 23:58:37 +0100

    Fin de l'intégration de la section affichant le code

--- web/ressources/js/social/displayFileCode.js
+++ web/ressources/js/social/displayFileCode.js
@@ -1,21 +1,33 @@
 $(function()
 {
-    var headerNavHtml = "<span class='glyphicon glyphicon-remove file-undisplay'></span>";
-    var headerNavCss = "<style>.file-undisplay { font-size: 1.3em; color: #000; } .home-problems { background-color: #ecf0f1; overflow: hidden; }</style>";
+    function resizeCode()
+    {
+        var headerHeight = $("div.file-display-header").outerHeight();
+        var containerHeight = $(".home-problems").outerHeight();
+        $("pre.display-code-tag").outerHeight(containerHeight-headerHeight);
+    }
     
 	function displayCode(name, code, fileElement)
 	{
+        var headerNavHtml = "<div class='file-display-header row'><p>"+name+"</p><span class='glyphicon glyphicon-remove file-undisplay'></span></div>";
+        
 		$(".home-content, .home-problems").css("width", "50%");
-		$(".home-problems").html(headerNavHtml + headerNavCss + "<pre class='display-code-tag prettyprint linenums'>");
+		$(".home-problems").html(headerNavHtml+"<pre class='display-code-tag prettyprint linenums row'></pre>");
 		$(".home-problems .display-code-tag").text(code);
         PR.prettyPrint();
         $(".file-container").addClass("file-unfocus");
         $(fileElement).closest(".file-container").removeClass("file-unfocus");
+        resizeCode();
 	}
+    
 	
 	$(".page-file-icon, .page-file-name").click(function() {
 		var name = $(this).closest(".file-container").data("file-name");
 		var code = $(this).closest(".file-container").data("file-code");
 		displayCode(name, code, this);
+	});
+    
+    $(window).resize(function() {
+		resizeCode();
 	});
 });
