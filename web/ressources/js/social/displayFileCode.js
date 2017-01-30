5e129dee0aa197c97b3348a76115b01f37b5381b
UlysseHall <ulysse.hall@hetic.net>
2017-01-30 15:41:14 +0100

    Récupération du code corrigé dans un input type hidden + popover sur le textarea. Input supprimé si displayCode() executé

--- web/ressources/js/social/displayFileCode.js
+++ web/ressources/js/social/displayFileCode.js
@@ -19,6 +19,10 @@ $(function()
     
 	function displayCode(name, code, fileElement)
 	{
+        $("div.label-code-saved").remove();
+        $("textarea.comment-input").popover("hide");
+        $("form.comment-form input[type=hidden]").remove();
+        
         var headerNavHtml = "<div class='file-display-header row'><p class='pb-code-edit'>Corriger ce code</p><p>"+name+"</p><span class='glyphicon glyphicon-remove file-undisplay'></span></div>";
         
 		$(".home-content, .home-problems").css("width", "50%");
@@ -42,18 +46,28 @@ $(function()
                 undisplayCode();
             });
             
+            resizeCode();
+            
             $("p.pb-code-edit").text("Valider").click(function() {
-                var codeEdited = $("pre.display-code-tag").text();
+                var codeEdited = $("pre.display-code-tag").text().replace(/'/g, "&#039;").replace(/"/g, "$quot;");
                 undisplayCode();
                 $(".form-confirmation").prepend("<div class='label label-success label-code-saved'>Correction enregistrée</div>");
+                $("form.comment-form").prepend("<input type='hidden' name='editedCodeContent' value='"+codeEdited+"'>").prepend("<input type='hidden' name='editedCodeName' value='"+name+"'>");
+                
+                $(".home-content").one("transitionend webkitTransitionEnd", function() {
+                    $("textarea.comment-input").popover({"title": "Correction enregistrée", "content": "Commentez et validez pour envoyer la correction", "placement": "top", "trigger": "manual"}).popover("show").click(function()
+                    {
+                        $(this).popover("hide");
+                    });
+                });
             })
         });
 	}
 	
 	$(".page-file-icon, .page-file-name").click(function() {
-		var name = $(this).closest(".file-container").data("file-name");
-		var code = $(this).closest(".file-container").data("file-code");
-		displayCode(name, code, this);
+        var name = $(this).closest(".file-container").data("file-name");
+        var code = $(this).closest(".file-container").data("file-code");
+        displayCode(name, code, this);
 	});
     
     $(window).resize(function() {
